e84daf656fc7427bed7a19301b5909d4d9e851a4
juanyanza11 <ww4fjj@gmail.com>
2019-05-02 10:34:23 -0500

    Subiendo run4 obtener la proyeccion del costo total de la carrera del estudiante

---
+++ miproyecto/run4.py
@@ -0,0 +1,41 @@
+'''
+	file: run4.py
+	autor: juanyanza11
+'''
+
+'''
+	Deseamos obtener el costo de una carrera univesitaria, el valor promedio de cada ciclo es de
+	1200 dólares, el valor promedio del seguro educativo es de POR CICLO 100 dólares si la edad del estudiantes
+	es menor o igual a 20 caso contrario será de 150 dólares si el estudiante tiene una modalidad a distancia
+	el numero de ciclos a seguir es de 10, caso contrario deberá seguir 8 ciclos. 
+
+	OBTENER LA PROYECCION DEL COSTO TOTAL DE LA CARRERA DEL ESTUDIANTE 
+
+'''
+from misvariables import *
+
+edad = input("Ingrese su edad\n")
+edad = int(edad)
+seguro1= 100
+seguro2=200
+valorc= 1200
+modalidad = input("Ingrese su modalidad\n")
+
+if (modalidad == "distancia") and (edad <=20):
+	costo = seguro1*10 + valorc*10
+	print("El costo de su matricula es: %s" % (costo))
+else:
+	if (modalidad == "distancia") and (edad >20):
+		costo = seguro2*10 + valorc*10
+		print("El costo de su matricula es: %s" % (costo))
+	else:
+		if (modalidad == "presencial") and (edad <=20):
+			costo = seguro1*8 + valorc*8
+			print("El costo de su matricula es: %s" % (costo))
+		else:
+			if (modalidad == "presencial") and (edad > 20):
+				costo = seguro2*8 +valorc*8
+				print("El costo de su matricula es: %s" % (costo))
+
+
+
